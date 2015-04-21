f63d947a3d03283e8fd01b106893a2e20b6d4360
Catherine Moresco <catherine.moresco@gmail.com>
2015-04-21 16:51:48 -0500

    minor cleanup; remove debug mode

--- bees.py
+++ bees.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-app.debug=True;
 
 import requests
 from bs4 import BeautifulSoup
@@ -17,8 +16,6 @@ def beenews():
       sl = hl.findNext('div')
       st = sl.findNext('div')
       curated_headlines[hl.text] = [hl.a.attrs.get(u'href').split('&')[0].split('/url?q=')[1], sl.text, st.text]
-
-  print curated_headlines
   return render_template('home.html', curated_headlines=curated_headlines)
 
 
