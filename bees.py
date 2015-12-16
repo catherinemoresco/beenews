c6099bb21da5f44d998ac89799cbbae6a3825e1d
Catherine Moresco <catherine.moresco@gmail.com>
2015-12-16 12:32:59 -0500

    added caching

--- bees.py
+++ bees.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
+from flask.ext.cache import Cache
+
 app = Flask(__name__)
+cache = Cache(app,config={'CACHE_TYPE': 'simple'})
 
 import requests
 from bs4 import BeautifulSoup
 
-@app.route("/")
+
+
+@cache.cached(timeout=43200)
 def beenews():
+  # cache to update once a day only (thanks, Google captcha)
+
   r = requests.get('https://www.google.com/search?hl=en&gl=us&tbm=nws&authuser=0&q=bees&oq=bees&gs_l=news-cc.3..43j0l10j43i53.5224.5630.0.5735.4.4.0.0.0.0.76.173.3.3.0...0.0...1ac.1.XVE1F-CxNM8')
   curated_headlines = {}
-  soup = BeautifulSoup(r.text)
+  soup = BeautifulSoup(r.text, "lxml")
 
   headlines = soup.find_all('h3')
   for hl in headlines:
@@ -16,8 +23,12 @@ def beenews():
       sl = hl.findNext('div')
       st = sl.findNext('div')
       curated_headlines[hl.text] = [hl.a.attrs.get(u'href').split('&')[0].split('/url?q=')[1], sl.text, st.text]
+  print curated_headlines
   return render_template('home.html', curated_headlines=curated_headlines)
 
+@app.route("/")
+def bees():
+  return beenews()
 
 if __name__ == "__main__":
     app.run(port=5001)
